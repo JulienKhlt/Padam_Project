036ca1dd593b17f1de462508cfaef81187b2ce8d
Julien KHLAUT <julien.khlaut@eleves.enpc.fr>
2021-02-09 23:13:36 +0100

    fixed

--- TSPTW.jl
+++ TSPTW.jl
@@ -7,21 +7,30 @@ function get_bound(people, n)
     return collect(people[u].start_time for u in 1:n), collect(people[u].end_time for u in 1:n)
 end
 
-function resolution_tsptw(n, people, map)
+function affichage(T, n)
+    for i in 1:n
+        println(string(i, " ", value(T[i])))
+    end    
+end
+
+function resolution_tsptw(n, people, map, M)
     # Pour l'instant un point = un client
     model = Model(GLPK.Optimizer)
 
     @variable(model, T[1:n] >= 0)
+    @variable(model, y[1:n, 1:n], Bin)
 
     a, b = get_bound(people, n)
 
-    @constraint(model, [i in 1:n, j in 1:n], T[i] - T[j] >= map[i, j])
-    @constraint(model, [i in 1:n, j in 1:n], T[j] - T[i] >= map[i, j])
+    @constraint(model, [i in 1:n, j in 1:n], T[i] - T[j] + M*y[i, j] >= map[i, j])
+    @constraint(model, [i in 1:n, j in 1:n], T[j] - T[i] - M*y[i, j] >= map[i, j] - M)
     @constraint(model, [i in 1:n], T[i] >= a[i])
     @constraint(model, [i in 1:n], T[i] <= b[i])
 
     @objective(model, Min, T[n])
     optimize!(model)
+
+    affichage(T, n)
 end
 
 Person(start_point = 5, start_time = 1000, end_time = 1000)
@@ -34,4 +43,4 @@ people = [Person(start_point = 1, start_time = 0, end_time = 1000),
 
 map = parser("Data/small.csv")
 
-resolution_tsptw(5, people, map)
+resolution_tsptw(5, people, map, 10000)
