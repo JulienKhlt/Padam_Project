b39823e3b9310c947855e93b27abb931ba76480d
Julien KHLAUT <julien.khlaut@eleves.enpc.fr>
2021-02-10 10:30:16 +0100

    tsptw general

--- TSPTW.jl
+++ TSPTW.jl
@@ -2,35 +2,46 @@ using JuMP, GLPK
 include("Person.jl")
 include("TimeTable.jl")
 
-function get_bound(people, n)
+function get_bound(people, nb_people)
     # TODO : reordonner
-    return collect(people[u].start_time for u in 1:n), collect(people[u].end_time for u in 1:n)
+    return collect(people[u].start_time for u in 1:nb_people), collect(people[u].end_time for u in 1:nb_people)
 end
 
-function affichage(T, n)
-    for i in 1:n
+function people_map(people, nb_people, map)
+    new_map = spzeros(nb_people, nb_people)
+    for i in 1:nb_people
+        for j in 1:nb_people
+            new_map[i, j] = map[people[i].start_point, people[j].start_point]
+        end
+    end
+    return new_map
+end
+
+function affichage(T, nb_people)
+    for i in 1:nb_people
         println(string(i, " ", value(T[i])))
     end    
 end
 
-function resolution_tsptw(n, people, map, M)
+function resolution_tsptw(nb_people, people, map, M)
     # Pour l'instant un point = un client
     model = Model(GLPK.Optimizer)
 
-    @variable(model, T[1:n] >= 0)
-    @variable(model, y[1:n, 1:n], Bin)
+    @variable(model, T[1:nb_people] >= 0)
+    @variable(model, y[1:nb_people, 1:nb_people], Bin)
 
-    a, b = get_bound(people, n)
+    a, b = get_bound(people, nb_people)
+    new_map = people_map(people, nb_people, map)
 
-    @constraint(model, [i in 1:n, j in 1:n], T[i] - T[j] + M*y[i, j] >= map[i, j])
-    @constraint(model, [i in 1:n, j in 1:n], T[j] - T[i] - M*y[i, j] >= map[i, j] - M)
-    @constraint(model, [i in 1:n], T[i] >= a[i])
-    @constraint(model, [i in 1:n], T[i] <= b[i])
+    @constraint(model, [i in 1:nb_people, j in 1:nb_people], T[i] - T[j] + M*y[i, j] >= new_map[i, j])
+    @constraint(model, [i in 1:nb_people, j in 1:nb_people], T[j] - T[i] - M*y[i, j] >= new_map[i, j] - M)
+    @constraint(model, [i in 1:nb_people], T[i] >= a[i])
+    @constraint(model, [i in 1:nb_people], T[i] <= b[i])
 
-    @objective(model, Min, T[n])
+    @objective(model, Min, T[nb_people])
     optimize!(model)
 
-    affichage(T, n)
+    affichage(T, nb_people)
 end
 
 Person(start_point = 5, start_time = 1000, end_time = 1000)
@@ -38,9 +49,8 @@ Person(start_point = 5, start_time = 1000, end_time = 1000)
 people = [Person(start_point = 1, start_time = 0, end_time = 1000), 
     Person(start_point = 2, start_time = 0, end_time = 1000),
     Person(start_point = 3, start_time = 0, end_time = 1000),
-    Person(start_point = 4, start_time = 0, end_time = 1000),
     Person(start_point = 5, start_time = 1000, end_time = 1000)]
 
 map = parser("Data/small.csv")
 
-resolution_tsptw(5, people, map, 10000)
+resolution_tsptw(4, people, map, 10000)
