a6820b540933b277f0e247a52d72a6bc80a8600b
Alice Krychowski <alicekrychowskimac@MacBook-Pro-de-Alice.local>
2021-04-12 16:36:39 +0200

    update plot pour ne voir que les arrets desservis

--- plot.jl
+++ plot.jl
@@ -50,9 +50,9 @@ function scatter_cluster_to_plot!(cluster::Cluster, localisations::Vector{Bus_st
     )
 end
 
+
 "Affiche le graphique des clusters présents dans une solution"
-function plot_clusters(solution::Solution, localisations::Vector{Bus_stop}, pl::Plots.Plot, scatter = false)::Plots.Plot
-    #pl = plot()
+function plot_clusters(solution::Solution, localisations::Vector{Bus_stop}, pl::Plots.Plot, scatter = true)::Plots.Plot
     for (index, cluster) in enumerate(solution.clusters)
         if scatter
             scatter_cluster_to_plot!(cluster, localisations, pl, index)
@@ -65,30 +65,11 @@ function plot_clusters(solution::Solution, localisations::Vector{Bus_stop}, pl::
 end
 
 
-"""
-Inputs :
-- localisations un vecteur d'éléments de type Bus_stop qui contient les localisations de tous les arrets de bus
-- drivers la liste des drivers (de type Person)
-- index_gare l'indice de la gare
-Affiche le graphique des arrets de bus, des dépots et de la gare
-Output : le graphe
-"""
-function plot_bus_stops(localisations::Vector{Bus_stop}, drivers::Vector{Person}, index_gare)::Plots.Plot
-    pl = plot()
-
-    #On affiche les arrets de bus
-    latitude_list = []
-    longitude_list = []
-    for i in 1:length(localisations)
-        bus_stop = localisations[i]
-        push!(latitude_list, bus_stop.latitude)
-        push!(longitude_list, bus_stop.longitude)
+"Affiche les dépots et la gare"
+function plot_terminus(localisations::Vector{Bus_stop}, drivers::Vector{Person}, index_gare, pl = nothing)::Plots.Plot
+    if pl == nothing
+        pl = plot()
     end
-    scatter!(
-        pl, latitude_list, longitude_list,
-        marker = (:circle, 3, 0.7, "black"),
-        label = "arrets de bus",
-    )
 
     #On affiche les dépots
     latitude_list = []
@@ -119,6 +100,35 @@ function plot_bus_stops(localisations::Vector{Bus_stop}, drivers::Vector{Person}
         markerstrokealpha = 0,
         label = "gare",
     )
+    plot!(title = "Carte des arrets de bus")
+    return pl
+end
+
+
+"""
+Inputs :
+- localisations un vecteur d'éléments de type Bus_stop qui contient les localisations de tous les arrets de bus
+- drivers la liste des drivers (de type Person)
+- index_gare l'indice de la gare
+Affiche le graphique des arrets de bus, des dépots et de la gare
+Output : le graphe
+"""
+function plot_bus_stops(localisations::Vector{Bus_stop}, drivers::Vector{Person}, index_gare)::Plots.Plot
+    pl = plot()
+    #On affiche les arrets de bus
+    latitude_list = []
+    longitude_list = []
+    for i in 1:length(localisations)
+        bus_stop = localisations[i]
+        push!(latitude_list, bus_stop.latitude)
+        push!(longitude_list, bus_stop.longitude)
+    end
+    scatter!(
+        pl, latitude_list, longitude_list,
+        marker = (:circle, 3, 0.7, "black"),
+        label = "arrets de bus",
+    )
+    plot_terminus(localisations, drivers, index_gare, pl)
     plot!(title = "Carte des arrets de bus")
     return pl
 end
